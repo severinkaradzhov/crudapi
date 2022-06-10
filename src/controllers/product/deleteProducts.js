3e18a9f348818c982ca7e13c7654ce7aba254532
severin <severin.karadzhov@strypes.eu>
2022-06-10 17:02:41 +0300

    Added temporary “trash” collection for deleted files

--- src/controllers/product/deleteProducts.js
+++ src/controllers/product/deleteProducts.js
@@ -1,9 +1,14 @@
 const functions = require('../../models/productModel')
+const addToTemp = require('../../models/temporaryModel')
 
 function deleteProduct(req, res) {
 	const id = req.params.id
 	functions.remove(id).then(result => {
 		if (result === null) return res.status(400).send({ msg: 'Product does not exist' })
+		result.date = 1
+		result.expireAfterSeconds = 604801
+		console.log(result)
+		addToTemp(result)
 		console.log('Deleted')
 		return res.status(200).send({ msg: 'Deleted' })
 	}).catch(err => {
---
+++ src/models/temporaryModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const temporarySchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	price: {
+		type: Number,
+		required: true
+	},
+	description: {
+		type: String,
+		required: false
+	},
+	sku: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	inStock: {
+		type: Number,
+		required: true,
+	},
+	brand: {
+		type: String,
+		required: false
+	},
+	date: {
+		type: Date,
+		required: true
+	}
+})
+
+const Temporary = mongoose.model('Temporary', temporarySchema)
+
+function addToTemp(product) {
+	Temporary.collection.createIndex({
+		date: 1,
+	}, {
+		expireAfterSeconds: 60 * 60 * 24,
+	})
+	const temp = {
+		_id: product._id,
+		name: product.name,
+		price: product.price,
+		sku: product.sku,
+		description: product.description,
+		inStock: product.inStock,
+		date: new Date(Date.now() - 60 * 60 * 24 * 1000)
+	}
+	if (product.brand) temp.brand = product.brand
+	console.log(temp)
+	return Temporary.collection.insertOne(temp)
+}
+
+module.exports = addToTemp
