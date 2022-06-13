6f0419d281b0b67ad046db434086a2bd0188ef42
severin <severin.karadzhov@strypes.eu>
2022-06-13 11:12:24 +0300

    Added query param search on name or description

--- src/controllers/product/deleteProducts.js
+++ src/controllers/product/deleteProducts.js
@@ -4,9 +4,7 @@ const addToTemp = require('../../models/temporaryModel')
 function deleteProduct(req, res) {
 	const id = req.params.id
 	functions.remove(id).then(result => {
-		if (result === null) return res.status(400).send({ msg: 'Product does not exist' })
-		result.date = 1
-		result.expireAfterSeconds = 604801
+		if (result === null) return res.status(404).send({ msg: 'Product does not exist' })
 		console.log(result)
 		addToTemp(result)
 		console.log('Deleted')
